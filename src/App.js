6c5f1851392ea4212ec9f8ea1a5d6cbbcd9b1ef3
majid amini <majidamini_s@yahoo.com>
2023-01-18 23:45:29 +0330

    operandfunction removed and eval calculation added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [currentDisplay, setCurrentDisplay] = useState([]);
-  const [prevDisplay, setPrevDisplay] = useState([]);
+  const [currentDisplay, setCurrentDisplay] = useState("");
+  const [prevDisplay, setPrevDisplay] = useState("");
 
+  const symbols = ['+','-','÷','*']
   const numbers = (e) => {
     let value = e.target.innerText;
-    setCurrentDisplay(currentDisplay + value);
+   
+    setPrevDisplay(prevDisplay + value);
   };
   const deleteHandler = (e) => {
     let newNumbers = currentDisplay.slice(0, -1);
     setCurrentDisplay(newNumbers);
   };
-  const operandHandler = (e) => {
-    let value = e.target.innerText;
-    setCurrentDisplay("");
-    if (value == "+") {
-      console.log(prevDisplay);
-      setPrevDisplay(prevDisplay + currentDisplay + value);
-    }
-    // if (value == "-") {
-    //   setPrevDisplay(eval(prevDisplay - currentDisplay));
-    // }
+
+  const calculate = () => {
+    setCurrentDisplay(eval(prevDisplay))
   };
   // const executeHandler = () => {};
   const clearHandler = () => {
@@ -34,8 +29,8 @@ function App() {
         <div className="displayContainer">
           <div className="displayTop"></div>
           <div className="displayBottom">
-            <span className="prevNumbers">{prevDisplay}</span>
-            <span className="currentNumbers">{currentDisplay}</span>
+            <span className="currentNumbers">{prevDisplay}</span>
+            <span className="prevNumbers">{currentDisplay}</span>
           </div>
         </div>
         <div className="operatorContainer">
@@ -46,23 +41,25 @@ function App() {
           <button>1/x</button>
           <button>x2</button>
           <button>logx</button>
-          <button onClick={operandHandler}>÷</button>
+          <button onClick={numbers}>÷</button>
           <button onClick={numbers}>7</button>
           <button onClick={numbers}>8</button>
           <button onClick={numbers}>9</button>
-          <button onClick={operandHandler}>*</button>
+          <button onClick={numbers}>*</button>
           <button onClick={numbers}>4</button>
           <button onClick={numbers}>5</button>
           <button onClick={numbers}>6</button>
-          <button onClick={operandHandler}>-</button>
+          <button onClick={numbers}>-</button>
           <button onClick={numbers}>1</button>
           <button onClick={numbers}>2</button>
           <button onClick={numbers}>3</button>
-          <button onClick={operandHandler}>+</button>
+          <button onClick={numbers}>+</button>
           <button>+/-</button>
           <button onClick={numbers}>0</button>
           <button>.</button>
-          <button className="equal">=</button>
+          <button onClick={calculate} className="equal">
+            =
+          </button>
         </div>
       </div>
     </div>
