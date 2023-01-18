29284dd9fd94617fd80e28285c85c073dd8ea159
majid amini <majidamini_s@yahoo.com>
2023-01-18 14:16:24 +0330

    deleteHandler function added for deleting the last item in string

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [currentDisplay,setCurrentDisplay] = useState([])
+  const [currentDisplay, setCurrentDisplay] = useState([]);
 
   const numbers = (e) => {
     let value = e.target.innerText;
-    setCurrentDisplay(currentDisplay + value)
-    
-  }
+    setCurrentDisplay(currentDisplay + value);
+  };
+  const deleteHandler = (e) => {
+    let newNumbers = currentDisplay.slice(0, -1);
+    setCurrentDisplay(newNumbers);
+  };
   return (
     <div className="App">
       <div className="calculatorContainer">
         <div className="displayContainer">
           <div className="displayTop"></div>
           <div className="displayBottom">
-            <span  className="prevNumbers">{currentDisplay}</span>
+            <span className="prevNumbers">{currentDisplay}</span>
             <span className="currentNumbers">0</span>
           </div>
         </div>
@@ -22,7 +25,7 @@ function App() {
           <button>%</button>
           <button>CE</button>
           <button>C</button>
-          <button>DEL</button>
+          <button onClick={deleteHandler}>DEL</button>
           <button>1/x</button>
           <button>x2</button>
           <button>logx</button>
@@ -38,11 +41,11 @@ function App() {
           <button onClick={numbers}>1</button>
           <button onClick={numbers}>2</button>
           <button onClick={numbers}>3</button>
-          <button >+</button>
-          <button >+/-</button>
+          <button>+</button>
+          <button>+/-</button>
           <button onClick={numbers}>0</button>
           <button>.</button>
-          <button>=</button>
+          <button className="equal">=</button>
         </div>
       </div>
     </div>
